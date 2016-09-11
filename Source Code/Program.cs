99eb7cf63a3a5020fd5dad327bfee9cf6c89855f
Io. D <i_-_-_s@outlook.com>
2016-09-11 22:56:23 +0300

    Initial Release

---
+++ Source Code/Program.cs
@@ -0,0 +1,28 @@
+﻿using ScriptExecutor.Interfaces;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace ScriptExecutor
+{
+    class Program
+    {
+        private const string ScriptsDirectory = @"";
+
+        static void Main(string[] args)
+        {
+            var compiledAssemblyPath = Path.Combine(Environment.CurrentDirectory, ScriptsDirectory);
+            var sourceFiles = Directory.EnumerateFiles(Environment.CurrentDirectory, "*.source", SearchOption.AllDirectories).ToArray();
+            var assembliesFiles = Directory.EnumerateFiles(Environment.CurrentDirectory, "*.assemblies", SearchOption.AllDirectories).ToArray();
+            var scriptAssembly = ScriptHelper.CompileAssembly(sourceFiles,assembliesFiles);
+            var scriptTypes = ScriptHelper.GetTypesImplementingInterface(scriptAssembly, typeof(IScript));
+            foreach (var scriptType in scriptTypes)
+            {
+                 var script = (IScript)Activator.CreateInstance(scriptType);
+               Console.WriteLine( script.Run());
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Source Code/ScriptHelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.CodeDom.Compiler;
+using System.Reflection;
+using Microsoft.CSharp;
+
+namespace ScriptExecutor
+{
+    public static class ScriptHelper
+    {
+        public static Assembly CompileAssembly(string[] sourceFiles,string[] assembliesFiles)
+        {
+            var codeProvider = new CSharpCodeProvider();
+  
+            var compilerParameters = new CompilerParameters
+            {
+                GenerateExecutable = false,
+                GenerateInMemory = true,
+                IncludeDebugInformation = false,
+            };
+
+            compilerParameters.ReferencedAssemblies.Add(Assembly.GetExecutingAssembly().Location);
+
+            string[] assemblies;
+
+            foreach (string assemblyFile in assembliesFiles)
+            {
+
+                assemblies =  File.ReadAllLines(assembliesFiles[0]);
+                foreach(string assembly in assemblies)
+                {
+                    if(assembly.Trim()!="")
+                    compilerParameters.ReferencedAssemblies.Add(assembly.Trim());
+                }
+            }
+            
+            
+            var result = codeProvider.CompileAssemblyFromFile(compilerParameters, sourceFiles);
+
+            if (result.Errors.HasErrors) throw new Exception("Assembly compilation failed.");
+
+            return result.CompiledAssembly;
+        }
+
+        public static List<Type> GetTypesImplementingInterface(Assembly assembly, Type interfaceType)
+        {
+            if (!interfaceType.IsInterface) throw new ArgumentException("Not an interface.", "interfaceType");
+
+            return assembly.GetTypes()
+                           .Where(t => interfaceType.IsAssignableFrom(t))
+                           .ToList();
+        }
+
+    }
+}
